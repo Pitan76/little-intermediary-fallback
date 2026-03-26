9b9fd63c4f44de4c9a4e1c47e98188ab4e2c3e4c
PTOM76 <ptms76@outlook.jp>
2026-03-26 18:43:08 +0900

    feat: add interfaces class_2350 and class_3908 with mixin implementations for Direction and MenuProvider

---
+++ src/main/java/net/pitan76/littleobffallback/mixin/DirectionMixin.java
@@ -0,0 +1,10 @@
+package net.pitan76.littleobffallback.mixin;
+
+import net.minecraft.class_2350;
+import net.minecraft.core.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Direction.class)
+public class DirectionMixin implements class_2350 {
+
+}
---
+++ src/main/java/net/pitan76/littleobffallback/mixin/MenuProviderMixin.java
@@ -0,0 +1,10 @@
+package net.pitan76.littleobffallback.mixin;
+
+import net.minecraft.class_3908;
+import net.minecraft.world.MenuProvider;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MenuProvider.class)
+public interface MenuProviderMixin extends class_3908 {
+
+}
